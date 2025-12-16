77e40ad5c7e6f053c42f0795fbffd06e3f6ae3df
josue santos <josue.santos.dev@gmail.com>
2025-12-16 16:47:48 -0300

    clean up code

--- pkg/tlv/parser.go
+++ pkg/tlv/parser.go
@@ -1,52 +1,39 @@
 package tlv
 
-import ("fmt"
- "strconv")
+import (
+	"fmt"
+	"strconv"
+)
 
 type TLV struct {
-	Pan string 
+	Pan          string
 	DataValidade string
-	CVM string
+	CVM          string
 }
 
 func (t *TLV) Parse(message string) error {
 	for _, tag := range IsoMessage {
-		if tag.Tag == "5A" {
-			tagLen := tag.Length+2
-			length := message[tag.Length:tagLen]
-			lenInteger, err := strconv.Atoi(length)
-			if err != nil {
-				return err
-			}
+		tagLen := tag.Length + 2
+		length := message[tag.Length:tagLen]
+		lenInteger, err := strconv.Atoi(length)
+		if err != nil {
+			return err
+		}
 
-			dataLen := lenInteger+tagLen
+		dataLen := lenInteger + tagLen
+
+		if tag.Tag == "5A" {
 			t.Pan = message[tagLen:dataLen]
 			message = message[dataLen:]
 		}
 
 		if tag.Tag == "5F24" {
-			tagLen := tag.Length+2
-			length := message[tag.Length:tagLen]
-			lenInteger, err := strconv.Atoi(length)
-			if err != nil {
-				return err
-			}
-
-			dataLen := lenInteger+tagLen
 			t.DataValidade = message[tagLen:dataLen]
 			message = message[dataLen:]
 		}
 
 		if tag.Tag == "9F34" {
-			tagLen := tag.Length+2
-			length := message[tag.Length:tagLen]
-			lenInteger, err := strconv.Atoi(length)
-			if err != nil {
-				return err
-			}
-			
-			dataLen := lenInteger+tagLen
-			t.CVM = message[tagLen:lenInteger+tagLen]
+			t.CVM = message[tagLen:dataLen]
 			message = message[tagLen:]
 		}
 
