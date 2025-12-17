28bc9e6e459827173432124029460d4a37fc46af
josue santos <josue.santos.dev@gmail.com>
2025-12-17 13:38:03 -0300

    feat: improve impl from parser

--- pkg/tlv/parser.go
+++ pkg/tlv/parser.go
@@ -2,46 +2,91 @@ package tlv
 
 import (
 	"fmt"
-	"strconv"
+	//"strconv"
 )
 
+type Parser struct {}
+
 type TLV struct {
-	Pan          string
-	DataValidade string
-	CVM          string
+	Tag   string
+	Len   int
+	Value []byte
 }
 
-func (t *TLV) Parse(message string) error {
-	for _, tag := range IsoMessage {
-		tagLen := tag.Length + 2
-		length := message[tag.Length:tagLen]
-		lenInteger, err := strconv.Atoi(length)
+func (p *Parser) Parse(data []byte) ([]TLV, error) {
+
+	var result []TLV
+	pos := 0
+
+	for pos < len(data) {
+
+		tag, usedTag, err := p.ParseTag(data[pos:])
 		if err != nil {
-			return err
+			return nil, err
+		}
+		pos += usedTag
+
+		L, usedLen, err := p.ParseLength(data[pos:])
+		if err != nil {
+			return nil, err
+		}
+		pos += usedLen
+
+		if pos+L > len(data) {
+			return nil, fmt.Errorf("value excede o buffer")
 		}
 
-		dataLen := lenInteger + tagLen
+		value := data[pos : pos+L]
+		pos += L
 
-		if tag.Tag == "5A" {
-			t.Pan = message[tagLen:dataLen]
-			message = message[dataLen:]
-		}
-
-		if tag.Tag == "5F24" {
-			t.DataValidade = message[tagLen:dataLen]
-			message = message[dataLen:]
-		}
-
-		if tag.Tag == "9F34" {
-			t.CVM = message[tagLen:dataLen]
-			message = message[tagLen:]
-		}
-
-		fmt.Println(message, 123)
+		result = append(result, TLV{
+			Tag:   fmt.Sprintf("%X", tag),
+			Len:   L,
+			Value: value,
+		})
 	}
-	return nil
+
+	return result, nil
 }
 
-func (t *TLV) String() string {
-	return fmt.Sprintf("%s %s %s", t.Pan, t.DataValidade, t.CVM)
+func (*Parser) ParseTag(data []byte) ([]byte, int, error) {
+
+	if len(data) == 0 {
+		return nil, 0, fmt.Errorf("buffer is null")
+	}
+
+	b1 := data[0]
+	tag := []byte{b1}
+	pos := 1
+
+	if (b1 & 0x1F) != 0x1F {
+		return tag, pos, nil
+	}
+
+	for {
+		if pos >= len(data) {
+			return nil, 0, fmt.Errorf("tag its worng")
+		}
+
+		b := data[pos]
+		tag = append(tag, b)
+		pos++
+
+		if (b & 0x80) == 0 {
+			break
+		}
+	}
+
+	return tag, pos, nil
+}
+
+
+func (*Parser) ParseLength(data []byte) (int, int, error) {
+
+	if len(data) < 1 {
+		return 0, 0, fmt.Errorf("lenght data is worng")
+	}
+
+	L := int(data[0])
+	return L, 1, nil
 }
